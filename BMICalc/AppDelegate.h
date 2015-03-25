5540d1a06504efca8affe10646f006b980f479ec
Justin Huntington <corusco@gmail.com>
2015-03-25 09:30:53 -0600

    Initial Commit

---
+++ BMICalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMICalc
+//
+//  Created by Justin Huntington on 3/25/15.
+//  Copyright (c) 2015 Cavalcade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMICalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMICalc
+//
+//  Created by Justin Huntington on 3/25/15.
+//  Copyright (c) 2015 Cavalcade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
